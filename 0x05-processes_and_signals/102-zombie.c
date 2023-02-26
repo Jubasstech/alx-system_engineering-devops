f8574fa6076e3dd61d76c403bca89fa273f6d5f1
Jerome Udoh <admin@jubass.com>
2023-02-26 02:46:54 -0800

    Write a C program that creates 5 zombie processes

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,40 +1,47 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 /**
- * infinite_while - runs an infinite loop for testing
+ * infinite_while - Run an infinite while loop.
  *
- * Return: always 0
+ * Return: Always 0.
  */
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+while (1)
+{
+sleep(1);
+}
+return (0);
 }
 
 /**
- * main - entry point for program
+ * main - Creates five zombie processes.
  *
- * Return: always 0
+ * Return: Always 0.
  */
 int main(void)
 {
-int i, pid;
+pid_t pid;
+char count = 0;
 
-for (i = 0; i < 5; i++)
+while (count < 5)
 {
 pid = fork();
-if (pid == -1)
-continue;
-else if (pid == 0)
-exit(0);
-else
+if (pid > 0)
+{
 printf("Zombie process created, PID: %d\n", pid);
+sleep(1);
+count++;
 }
+else
+exit(0);
+}
+
 infinite_while();
-return (0);
+
+return (EXIT_SUCCESS);
 }
